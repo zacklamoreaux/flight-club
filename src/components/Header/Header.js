5a449af0ca7ae40253b4e84ca29d166c888fdcbf
Zack <zacklam42@gmail.com>
2018-04-23 21:05:06 -0600

    smch

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
       <div>
         <div className='top'>
           <img src='https://www.flightclub.com/media/vaimo/uploadlogo/default/FC.png' alt='logo'/>
-          <input type='text' />
+          <input type='text' className='search-bar' />
         </div>
         <div className='bottom'>
 
